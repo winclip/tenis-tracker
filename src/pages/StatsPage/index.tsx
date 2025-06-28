87594d8da9bf1c920a24691580e1465d9e1d793a
winclip <sokhotnikoff@gmail.com>
2025-06-28 23:14:17 +0200

    feat(stats): add "Home" button to navigate back to main page

--- src/pages/StatsPage/index.tsx
+++ src/pages/StatsPage/index.tsx
@@ -1,11 +1,23 @@
+import { useNavigate } from "react-router-dom";
 import SetsHistoryDisplay from "../../components/SetsHistoryDisplay";
 import StatsDisplay from "../../components/StatsDisplay";
+import { Button } from "antd";
 
 export default function StatsPage() {
+  const navigate = useNavigate();
+
   return (
     <>
       <SetsHistoryDisplay />
       <StatsDisplay />
+      <Button
+        type="primary"
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        Home
+      </Button>
     </>
   );
 }
